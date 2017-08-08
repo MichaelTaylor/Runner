769d2804b55d9b5192ed49f39f1554708956c2a4
Michael Taylor <michael.taylor.gamedev@gmail.com>
2017-08-08 14:49:49 -0500

    First few steps

---
+++ Runner/Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour {
+
+    public static GameplayManager instance;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    private void Singleton()
+    {
+        if (GameplayManager.instance == null)
+        {
+            GameplayManager.instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Runner/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+    public float knockbackForce;
+
+    private Rigidbody2D RB2D;
+
+    // Use this for initialization
+    private void Start ()
+    {
+        GetComponents();
+	}
+
+    private void GetComponents()
+    {
+        RB2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        if (!Input.anyKey) return;
+        InputControl();
+	}
+
+    private void InputControl()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void MoveForward()
+    {
+
+    }
+
+    private void Jump()
+    {
+        RB2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    public void KnockBack()
+    {
+        
+    }
+}
